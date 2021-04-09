4022072fd782ebc55876bfd0f9b3510001669872
alejoshua2000 <jlopezh@ucenfotec.ac.cr>
2021-04-08 19:23:00 -0600

    Route y model para el modelo de registro de proveedores.

---
+++ proyectopipasapi/models/registroProveedor.model.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema_registroProveedor = new mongoose.Schema({
+    tipoProveedor: { type: String, required: true, unique: false },
+    tipoServicio: { type: String, required: true, unique: false },
+    tipoIdentificacion: { type: String, required: true, unique: false },
+    pAcargo: { type: String, required: true, unique: false },
+    empresa: { type: String, required: true, unique: false },
+    contraEmpresa: { type: String, required: true, unique: false },
+    telefono: { type: String, required: true, unique: false },
+    correo: { type: String, required: true, unique: false },
+    provincias: { type: Number, required: true, unique: false },
+    cantones: { type: Number, required: true, unique: false },
+    distritos: { type: Number, required: true, unique: false },
+    comentarios: { type: String, required: true, unique: false },
+    myFile: { type: String, required: true, unique: false }
+
+});
+
+module.exports = mongoose.model('RegistroProveedor', schema_registroProveedor, 'RegistroProveedor');
---
+++ proyectopipasapi/routes/registroProveedor.route.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const RegistroProveedor = require('../models/registroProveedor.model');
+
+router.post('/registrar-proveedor', (req, res) => {
+    let nuevo_proveedor = new RegistroProveedor({
+        tipoProveedor: req.body.tipoProveedor,
+        tipoServicio: req.body.tipoServicio,
+        tipoIdentificacion: req.body.tipoIdentificacion,
+        pAcargo: req.body.pAcargo,
+        empresa: req.body.empresa,
+        contraEmpresa: req.body.contraEmpresa,
+        telefono: req.body.telefono,
+        correo: req.body.correo,
+        provincias: req.body.provincias,
+        cantones: req.body.cantones,
+        distritos: req.body.distritos,
+        comentarios: req.body.comentarios,
+        myFile: req.body.myFile,
+    });
+
+    nuevo_proveedor.save((err, proveedor_db) => {
+        if (err) {
+            res.json({
+                msj: "No se pudo registrar su perfil",
+                err
+            });
+
+        } else {
+            res.json({
+                msj: "Sus registros se guardaron exitosamente",
+                proveedor_db
+            });
+        }
+    });
+
+});
+
+router.get('/listar-proveedor', (req, res) => {
+    RegistroProveedor.find((err, lista_proveedor) => {
+        if (err) {
+            res.json({
+                msj: "No se pudo encontrar su perfil",
+                err
+            });
+
+        } else {
+            res.json({ lista_proveedor });
+        }
+    });
+
+});
+
+module.exports = router;
