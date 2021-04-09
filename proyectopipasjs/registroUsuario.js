c3899989f0da6eaae0b295dce20c07d8ced33bf6
AnaSalas <asalasv@ucenfotec.ac.cr>
2021-04-08 14:10:02 -1200

    Registrousuario. Agreg tarjeta, ven y tipo cedula

--- proyectopipasjs/registroUsuario.js
+++ proyectopipasjs/registroUsuario.js
@@ -1,6 +1,7 @@
 'use strict'
 const nombreUsuario = document.querySelector('#nombre');
 const apellidoUsuario = document.querySelector("#apellidos");
+const tipoIdentificacionUsuario = document.querySelector("#tipoIdentificacion");
 const identificacionUsuario = document.querySelector("#identificacion");
 const fechaNacimientoUsuario = document.querySelector("#fechaNacimiento");
 const provinciasUsuario = document.querySelector("#provincias");
@@ -10,12 +11,15 @@ const generoUsuario = document.querySelector("#genero");
 const cantidadMascotaUsuario = document.querySelector("#cantidadMascota");
 const telefonoContactoUsuario = document.querySelector("#telefonoContacto");
 const correoElectronicoUsuario = document.querySelector("#correoElectronico");
+const numeroTarjeta = document.querySelector("#numTarjeta");
+const fechaVencimiento = document.querySelector("#fechaVencimiento");
 const condicionesUsuario = document.querySelector('#condiciones');
 const botonEnviar = document.querySelector("#btnSubmit");
 
 const obtenerDatos = () => {
     let nombre = nombreUsuario.value;
     let apellidos = apellidoUsuario.value;
+    let tipoIdentificacion = tipoIdentificacionUsuario.value;
     let identificacion = identificacionUsuario.value;
     let fechaNacimiento = fechaNacimientoUsuario.value;
     let provincia = provinciasUsuario.value;
@@ -25,10 +29,13 @@ const obtenerDatos = () => {
     let cantidadMascota = cantidadMascotaUsuario.value;
     let telefonoContacto = telefonoContactoUsuario.value;
     let correoElectronico = correoElectronicoUsuario.value;
+    let numTarjeta = numeroTarjeta.value;
+    let vencimiento = fechaVencimiento.value;
 
     document.cookie = "tipoperfil=U";
     document.cookie = "nombreUsuario =" + nombre;
     document.cookie = "apellidos=" + apellidos;
+    document.cookie = "identificacion =" + tipoIdentificacion;
     document.cookie = "identificacionUsuario =" + identificacion;
     document.cookie = "fechaNacimiento=" + fechaNacimiento;
     document.cookie = "provinciaUsuario=" + provincia;
@@ -38,10 +45,13 @@ const obtenerDatos = () => {
     document.cookie = "cantidadMascota=" + cantidadMascota;
     document.cookie = "telefonoUsuario=" + telefonoContacto;
     document.cookie = "correo=" + correoElectronico;
+    document.cookie = "numeroTarjeta=" + numTarjeta;
+    document.cookie = "fechaVencimientoTarjeta=" + vencimiento;
     document.cookie = "password=" + "contraSena22!";
 
     console.log('El nombre del usuario es es: ' + nombre);
     console.log('Los apellidos del usuario es es: ' + apellidos);
+    console.log('El tipo de identificación es: ', tipoIdentificacion);
     console.log('La cédula del usuario es es: ' + identificacion);
     console.log('La fecha de Nacimiento del usuario es es: ' + fechaNacimiento);
     console.log('La Provincia es: ' + provincia);
@@ -51,6 +61,8 @@ const obtenerDatos = () => {
     console.log('Cantidad de mascotas: ' + cantidadMascota);
     console.log('Eltelefono del usuario es : ' + telefonoContacto);
     console.log('El correo electronico  del usuario es : ' + correoElectronico);
+    console.log('El numero de tarjeta es: ' + numTarjeta);
+    console.log('La fecha de vencimiento de la tarjeta es; ' + vencimiento);
     Swal.fire({
         'icon': 'success',
         'title': 'Hemos recibido su solicitud de registro',
@@ -62,6 +74,7 @@ const obtenerDatos = () => {
 const limpiar = () => {
     nombreUsuario.value = "";
     apellidoUsuario.value = "";
+    tipoIdentificacionUsuario.value = "";
     identificacionUsuario.value = "";
     provinciasUsuario.value = "";
     cantonesUsuario.value = "";
@@ -70,6 +83,8 @@ const limpiar = () => {
     fechaNacimientoUsuario.value = "";
     telefonoContactoUsuario.value = "";
     correoElectronicoUsuario.value = "";
+    numeroTarjeta.value = "";
+    fechaVencimiento.value = "";
 }
 
 const validar = () => {
@@ -97,6 +112,14 @@ const validar = () => {
     } else {
         correoElectronicoUsuario.classList.remove('error-input');
     }
+    let regExp_numeroTarjeta = /4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}/;
+    if (!regExp_numeroTarjeta.test(numeroTarjeta.value)) {
+        error = true;
+        numeroTarjeta.classList.add('error-input');
+    } else {
+        numeroTarjeta.classList.remove('error-input');
+    }
+
     if (error == false) {
         obtenerDatos();
     } else {
