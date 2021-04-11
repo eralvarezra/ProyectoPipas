708100fdee22ea1143a15d8727e7599ad1c4cc93
ErickAlvarez <ealvarezr@ucenfotec.ac.cr>
2021-04-10 22:29:54 -0600

    listar caracteristicas especiales

---
+++ proyectopipasjs/listar-caracteristicas.js
@@ -0,0 +1,81 @@
+'use strict'
+
+//obteniendo los elementos de HTML
+const tabla = document.querySelector("#tbl-resultados tbody");
+//Filtro
+const input_filtro = document.querySelector("#txt-filtro");
+
+const mostrar_modal_editar = async(caracteristica) => {
+        const { value: formValues } = await Swal.fire({
+            title: 'Editar Caracteristica',
+            html: `
+            <div>
+                <label for="txt-nombre">Nombre:</label>
+                <input type="text" id="txt-general" required class="swal2-input" value="${caracteristica.nombreCaracteristica}">
+                </select>
+            </div>`,
+            focusConfirm: false,
+            preConfirm: () => {
+                return [
+                    caracteristica._id,
+                    document.querySelector('#txt-general').value,
+                ]
+            }
+        });
+        if (formValues) {
+            const { value: accept } = await Swal.fire({
+                icon: 'warning',
+                text: 'Está seguro que desea modificar la caracteristica',
+                confirmButtonText: `Si`,
+                showCancelButton: true
+            });
+            if (accept) {
+                modificar_caracteristica(formValues[0]);
+            }
+        }
+    }
+    //Tabla dinamica
+const mostrar_caracteristica = async() => {
+    let lista_caracteristicas = await listar_caracteristicas();
+    console.log(lista_caracteristicas);
+    tabla.innerHTML = '';
+    let filtro = input_filtro.value.toUpperCase();
+
+    lista_caracteristicas.forEach((caracteristica) => {
+        console.log(caracteristica);
+        if (caracteristica.nombreCaracteristica.toUpperCase().includes(filtro)) {
+
+            let fila = tabla.insertRow();
+            fila.insertCell().innerHTML = caracteristica.nombreCaracteristica;
+
+            let celda_editar = fila.insertCell();
+            let boton_editar = document.createElement('button');
+            boton_editar.classList.add("far")
+            boton_editar.classList.add("fa-edit")
+            boton_editar.type = 'button';
+
+            boton_editar.addEventListener('click', async() => {
+                mostrar_modal_editar(caracteristica);
+            })
+            celda_editar.appendChild(boton_editar);
+
+            //ELIMINAR:
+            let celda_eliminar = fila.insertCell();
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+
+            boton_eliminar.classList.add("far")
+            boton_eliminar.classList.add("fa-trash-alt")
+
+            celda_eliminar.appendChild(boton_eliminar);
+
+            celda_eliminar.addEventListener('click', async() => {
+                eliminar_caracteristica(caracteristica._id)
+            });
+        }
+    });
+};
+
+mostrar_caracteristica();
+
+input_filtro.addEventListener('keyup', mostrar_caracteristica);
--- proyectopipasjs/proyectopipas.servicios.js/registrocaracteristica-servicio.js
+++ proyectopipasjs/proyectopipas.servicios.js/registrocaracteristica-servicio.js
@@ -21,3 +21,66 @@ const registrar_caracteristica = async(nombreCaracteristica, fechaCreacion) => {
         });
     });
 };
+const listar_caracteristicas = async() => {
+    let lista_caracteristicas = [];
+    await axios({
+        method: 'get',
+        url: 'http://localhost:3000/api/listar-caracteristicas',
+        responseType: 'json'
+    }).then((response) => {
+        lista_caracteristicas = response.data.lista_caracteristicas;
+    }).catch((response) => {
+        console.log(response.data.msj + " " + response.data.err);
+    });
+    return lista_caracteristicas;
+}
+
+const modificar_caracteristica = async(_id, nombreCaracteristica) => {
+    await axios({
+        method: 'put',
+        url: 'http://localhost:3000/api/modificar-caracteristicas',
+        responseType: 'json',
+        data: {
+            _id: _id,
+            nombreCaracteristica: nombreCaracteristica
+        }
+    }).then((response) => {
+        Swal.fire({
+            'icon': 'success',
+            'title': 'La caracteristica ha sido actualizada',
+            'text': response.msj
+        }).then(() => {
+            mostrar_caracteristicas();
+        });
+    }).catch((response) => {
+        Swal.fire({
+            'icon': 'error',
+            'text': response.msj,
+            'title': 'Ocurrió un error inesperado',
+        }).then(() => {});
+    });
+};
+const eliminar_caracteristica = async(_id) => {
+    await axios({
+        method: 'delete',
+        url: 'http://localhost:3000/api/eliminar-caracteristica',
+        responseType: 'json',
+        data: {
+            _id: _id
+        }
+    }).then((response) => {
+        Swal.fire({
+            'title': 'La característica ha sido eliminada',
+            'icon': 'success',
+            'text': response.msj
+        }).then(() => {
+            mostrar_caracteristicas();
+        });
+    }).catch((response) => {
+        Swal.fire({
+            'title': response.msj,
+            'icon': 'error',
+            'text': response.err
+        })
+    });
+};
