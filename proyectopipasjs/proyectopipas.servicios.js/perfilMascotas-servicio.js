9f46a5fd74bd06f08acb0e1efa0a7fc0ed3884d8
alejoshua2000 <jlopezh@ucenfotec.ac.cr>
2021-04-17 22:07:55 -0600

    listar mascota listo

--- proyectopipasjs/proyectopipas.servicios.js/perfilMascotas-servicio.js
+++ proyectopipasjs/proyectopipas.servicios.js/perfilMascotas-servicio.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const obtener_mascotas = async() => { // esta es la funcion que se usa en el controlador, que llama la funcion
-    let lista_mascota = [];
+const listar_mascotas = async() => { // esta es la funcion que se usa en el controlador, que llama la funcion
+    let lista_mascotas = [];
     await axios({
         method: 'get',
         url: 'http://localhost:3000/api/listar-mascota', // con este link se puede probar si la collection es visible en postman
         responseType: 'json',
     }).then((response) => {
-        lista_mascota = response.data.lista_mascota;
+        lista_mascotas = response.data.lista_mascotas;
     }).catch((response) => {
         console.log(response.data.err);
     });
-    return lista_mascota;
+    return lista_mascotas;
 };
--- proyectopipasjs/verPerfilMascotas.js
+++ proyectopipasjs/verPerfilMascotas.js
@@ -6,12 +6,7 @@ const razaMascota = document.querySelector('#tipoRaza');
 const caracteristicaEspecialMascota = document.querySelector('#caracteristicaEspecial');
 const caracteristicaPadecimientoMascota = document.querySelector('#tipoPadecimiento');
 const caracteristicaVacunaMascota = document.querySelector('#tipoVacuna');
-const botonNueva = document.querySelector('#btnNueva');
-const botonEliminar = document.querySelector('#btnEliminar');
-const botonEnviar = document.querySelector('#btnGuardar');
-const botonConfirmar = document.querySelector("#btnConfirmar");
-const botonVolver = document.querySelector("#btnVolver");
-const popupConfirmar = document.querySelector("#sct-EliminarMascota");
+const tabla = document.getElementById('tabladinamica');
 
 function readCookie(pCookie) {
     const nameString = pCookie + "="
@@ -27,165 +22,37 @@ function readCookie(pCookie) {
     }
 }
 
-const inicializar_perfil = () => {
-    let correo = readCookie("correo");
-    let lista_actual = document.getElementById('nombreMascota');
-    let listaMascotas = obtener_mascotas();
-    console.log(listaMascotas);
-    console.log(lista_actual);
-    console.log(correo);
+const mostrar_mascotas = async() => {
+    let lista_mascota = await listar_mascotas();
+    let filtro = readCookie("correo");
+    filtro = filtro.replace("=", "");
+    console.log(lista_mascota, filtro);
+    tabla.innerHTML = '';
 
+    lista_mascota.forEach((mascota) => {
+        console.log(mascota);
+        if (mascota.correo === filtro) {
 
-}
+            let fila = tabla.insertRow();
+            fila.insertCell().innerHTML = (mascota.nombreMascota);
+            fila.insertCell().innerHTML = (mascota.tipoMascota);
+            fila.insertCell().innerHTML = (mascota.tipoRaza);
+            fila.insertCell().innerHTML = (mascota.fotoMascota);
+            fila.insertCell().innerHTML = (mascota.caracteristicaEspecial);
+            fila.insertCell().innerHTML = (mascota.tipoPadecimiento);
+            fila.insertCell().innerHTML = (mascota.tipoVacuna);
 
-const obtenerDatos = () => {
-    let nombreMascota = nombreMascotaU.value;
-    let tipoMascota = tipoMascotaU.value;
-    let raza = razaMascota.value;
-    let caracteristicaEspecial = caracteristicaEspecialMascota.value;
-    let caracteristicaPadecimiento = caracteristicaPadecimientoMascota.value;
-    let caracteristicaVacuna = caracteristicaVacunaMascota.value;
-    console.log('El  nombre de la mascota es : ' + nombreMascota);
-    console.log('El  tipo de mascota es : ' + tipoMascota);
-    console.log('La raza de la mascota es : ' + raza);
-    console.log('Las caracteristicas especiales de las mascotas son : ' + caracteristicaEspecial);
-    console.log('Los padecimientos de las mascota son  : ' + caracteristicaPadecimiento);
-    console.log('Las vacunas de las mascota son  : ' + caracteristicaVacuna);
-    Swal.fire({
-        'icon': 'success',
-        'title': 'Éxito!',
-        'text': 'Su cambio a sido guardado exitosamente'
+            //Eliminar
+            let celda_eliminar = fila.insertCell();
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+
+            boton_eliminar.classList.add("far")
+            boton_eliminar.classList.add("fa-trash-alt")
+
+            celda_eliminar.appendChild(boton_eliminar);
+        }
     });
 }
 
-const validar = () => {
-    let error = false;
-    let campos_requeridos = document.querySelectorAll(':required');
-    campos_requeridos.forEach(campo => {
-        if (campo.value == '') {
-            error = true;
-            campo.classList.add('error-input');
-        } else {
-            campo.classList.remove('error-input');
-
-        }
-
-    })
-    if (error == false) {
-        obtenerDatos();
-    }
-}
-
-const btnAgregar = document.getElementById("btnAgregar");
-const btnRemover = document.getElementById("btnRemover");
-
-const btpAgregar = document.getElementById("btpAgregar");
-const btpRemover = document.getElementById("btpRemover");
-
-const btvAgregar = document.getElementById("btvAgregar");
-const btvRemover = document.getElementById("btvRemover");
-
-var counter = 0;
-var counterp = 0;
-var counterv = 0;
-
-const nueva = () => {
-    location.href = "verPerfilMascota.html";
-}
-
-const eliminar = () => {
-    popupConfirmar.style.display = "block";
-
-}
-const volver = () => {
-    popupConfirmar.style.display = "none"
-}
-
-const confirmar = () => {
-    location.href = "perfil.html"
-}
-
-const agregarCampos = () => {
-    if (counter < 6) {
-        counter++;
-        var newFields = document.getElementById('caracteristicaEspecial').cloneNode(true);
-        newFields.id = 'caracteristicaEspecial' + counter;
-        newFields.style.display = 'block';
-        newFields.style.textAlign = "center";
-        newFields.style.marginLeft = "180px";
-        document.querySelector("#div-Form").style.height = "1420px";
-
-        document.getElementById("selectTipoCaracteristica").appendChild(newFields);
-    }
-}
-
-const agregarCamposP = () => {
-    if (counterp < 8) {
-        counterp++;
-        var newFields = document.getElementById('tipoPadecimiento').cloneNode(true);
-        newFields.id = 'tipoPadecimiento' + counterp;
-        newFields.style.display = 'block';
-        newFields.style.textAlign = "center";
-        newFields.style.marginLeft = "180px";
-        document.querySelector("#div-Form").style.height = "1420px";
-
-        document.getElementById("selectTipoPadecimiento").appendChild(newFields);
-    }
-}
-
-const agregarCamposV = () => {
-    if (counterv < 8) {
-        counterv++;
-        var newFields = document.getElementById('tipoVacuna').cloneNode(true);
-        newFields.id = 'tipoVacuna' + counterv;
-        newFields.style.display = 'block';
-        newFields.style.textAlign = "center";
-        newFields.style.marginLeft = "180px";
-        document.querySelector("#div-Form").style.height = "1420px";
-
-        document.getElementById("selectTipoVacuna").appendChild(newFields);
-    }
-}
-
-
-
-const removerCampos = () => {
-    if (counter > 0) {
-        var item = document.getElementById("selectTipoCaracteristica").lastChild
-        document.getElementById("selectTipoCaracteristica").removeChild(item);
-        counter--;
-    }
-}
-
-const removerCamposP = () => {
-    if (counterp > 0) {
-        var item = document.getElementById("selectTipoPadecimiento").lastChild
-        document.getElementById("selectTipoPadecimiento").removeChild(item);
-        counterp--;
-    }
-}
-
-const removerCamposV = () => {
-    if (counterv > 0) {
-        var item = document.getElementById("selectTipoVacuna").lastChild
-        document.getElementById("selectTipoVacuna").removeChild(item);
-        counterv--;
-    }
-}
-
-btnAgregar.addEventListener('click', agregarCampos);
-btnRemover.addEventListener('click', removerCampos);
-
-btpAgregar.addEventListener('click', agregarCamposP);
-btpRemover.addEventListener('click', removerCamposP);
-
-btvAgregar.addEventListener('click', agregarCamposV);
-btvRemover.addEventListener('click', removerCamposV);
-
-botonEnviar.addEventListener('click', validar);
-botonNueva.addEventListener('click', nueva);
-botonEliminar.addEventListener('click', eliminar);
-botonConfirmar.addEventListener("click", confirmar)
-botonVolver.addEventListener('click', volver);
-
-window.onload = inicializar_perfil;
+mostrar_mascotas();
