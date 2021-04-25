6947b49fc01759d3c7220a398c8c68855eeb970b
Yin28 <syangg@ucenfotec.ac.cr>
2021-04-24 21:08:37 -0600

    registroUsuario-correo mensaje

--- proyectopipasapi/routes/proyectopipastemplates/registroUsuario-correo.js
+++ proyectopipasapi/routes/proyectopipastemplates/registroUsuario-correo.js
@@ -18,12 +18,12 @@ this.enviar_email = (pnombre_completo, pcorreo) => {
         to: pcorreo,
         subject: `Bienvenido a PetLover ${pnombre_completo}`,
         html: `
-        <table border="2" cellpadding="0" cellspacing="0" width="600px" background-color="rgb(204,73,79)" ; bgcolor="#e69138" ;>
-        <tr height=" 70px ">
-            <td width="600px ">
-                <h1 style="color: rgb(5, 4, 4); text-align:center ">Bienvenido a PetLover</h1>
-                <p style="color: #fff; text-align:center ">
-                    <span style="color: rgb(204, 73, 79);;font-weight: bold; ">${pnombre_completo}</span> esperamos le pueda sacar provecho a la aplicación.
+        <table cellpadding="0" cellspacing="0" width="600px" background-color="rgb(255,255,255)" ; bgcolor="#cc494f" ;>
+            <tr height=" 70px ">
+                <td width="600px ">
+                    <h1 style="color:#fff; text-align:center ">Bienvenido a PetLover</h1>
+                    <p style="color: #fff; text-align:center ">
+                    <span style="color:#fff;font-weight: bold; ">${pnombre_completo}</span> esperamos le pueda sacar provecho a la aplicación.
                 </p>
             </td>
         </tr>
