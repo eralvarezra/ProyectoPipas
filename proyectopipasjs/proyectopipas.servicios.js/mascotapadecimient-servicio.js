da27e6c6460508300649c6d6d89d9fe833ed47a7
alejoshua2000 <jlopezh@ucenfotec.ac.cr>
2021-04-24 22:53:46 -0600

    cambios a mascotas padecimientos

--- proyectopipasjs/proyectopipas.servicios.js/mascotapadecimient-servicio.js
+++ proyectopipasjs/proyectopipas.servicios.js/mascotapadecimient-servicio.js
@@ -3,7 +3,7 @@
 const registrar_mascotapadecimiento = async(correo, nombreMascota, nombrePadecimiento) => {
     await axios({
         method: 'post',
-        url: 'http://localhost:3000/api/registrar-mascotavacuna',
+        url: 'http://localhost:3000/api/registrar-mascotapadecimiento',
         responseType: 'json',
         data: {
             correo: correo,
@@ -29,7 +29,7 @@ const lista_mascotapadecimiento = async() => {
     let lista_mascotapadecimiento = [];
     await axios({
         method: 'get',
-        url: 'http://localhost:3000/api/listar-MascotaVacuna',
+        url: 'http://localhost:3000/api/listar-/registrar-mascotapadecimiento',
         responseType: 'json'
     }).then((response) => {
         lista_mascotapadecimiento = response.data.lista_mascotapadecimiento;
@@ -42,16 +42,16 @@ const lista_mascotapadecimiento = async() => {
 const modificar_mascotapadecimiento = async(_id, nombreVacuna) => {
     await axios({
         method: 'put',
-        url: 'http://localhost:3000/api/modificar-mascotavacunas',
+        url: 'http://localhost:3000/api/modificar-/registrar-mascotapadecimiento',
         responseType: 'json',
         data: {
             _id: _id,
-            nombreVacuna: nombreVacuna
+            nombrePadecimiento: nombrePadecimiento
         }
     }).then((response) => {
         Swal.fire({
             'icon': 'success',
-            'title': 'La vacuna ha sido actualizada',
+            'title': 'El padecimiento ha sido actualizado',
             'text': response.msj
         }).then(() => {
             mostrar_vacuna();
@@ -67,7 +67,7 @@ const modificar_mascotapadecimiento = async(_id, nombreVacuna) => {
 const eliminar_mascotapadecimiento = async(_id) => {
     await axios({
         method: 'delete',
-        url: 'http://localhost:3000/api/eliminar-Mascotavacuna',
+        url: 'http://localhost:3000/api/eliminar-mascotapadecimiento',
         responseType: 'json',
         data: {
             _id: _id
